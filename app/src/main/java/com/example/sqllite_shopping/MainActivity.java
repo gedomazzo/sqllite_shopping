d3a38298c275da3259453879af3050ab5d1318d2
Vasya <br2876@bs.amalnet.k12.il>
2026-04-07 13:31:03 +0300

    checking is the input is wrong

--- app/src/main/java/com/example/sqllite_shopping/MainActivity.java
+++ app/src/main/java/com/example/sqllite_shopping/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sqllite_shopping;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     String Smon;
     int Scat;
     String Sdat;
-    String[] categories = {"select category", "food", "clothes", "electronics", "drugs", "other"};
+    String[] categories = {"select category:", "food", "clothes", "electronics", "drugs", "other"};
 
     public void merrage() {
         des = findViewById(R.id.des);
@@ -48,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categories){
             @Override
             public boolean isEnabled(int position) {
-                return position != 0; // disable first item
+                return position != 0;
             }
         };
         cat.setAdapter(adapter);
@@ -77,14 +78,63 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             Log.i("error", Sdat);
             Log.i("error", String.valueOf(Scat));
 
-            // allert going here
-        }
+
+            AlertDialog.Builder err = new AlertDialog.Builder(this);
+
+            err.setTitle("Oops, something is wrong");
+            err.setMessage("Please check your input");
+            err.setPositiveButton("Ok", (dialogInterface, i) -> {
+                dialogInterface.cancel();
+            });
+            err.show();
+
+            return;
+            }
+
+        
+
+
 
 
     }
 
 
     public static boolean CheckDate(String date){
-        return true;
+
+        String[] split = date.split("/");
+
+        if ((Integer.parseInt(split[1]) > 12) || (Integer.parseInt(split[1]) == 0)){
+            return true;
+        }
+
+        if (split.length == 3) {
+            int day = Integer.parseInt(split[0]);
+            int month = Integer.parseInt(split[1]);
+            int year = Integer.parseInt(split[2]);
+
+            if ((month == 4) || (month == 6) || (month == 9) || (month == 11)) {
+                if (day > 30) {
+                    return true;
+                }
+            } else if (month == 2) {
+                if (year % 4 == 0) {
+                    if (day > 29) {
+                        return true;
+                    }
+                } else {
+                    if (day > 28) {
+                        return true;
+                    }
+                }
+            } else {
+                if (day > 31) {
+                    return true;
+                }
+            }
+        } else return true;
+
+        return false;
     }
+
+
 }
