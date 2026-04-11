d5156a72d1b893a0d9cd0e9ea8684950cd276852
Vasya <br2876@bs.amalnet.k12.il>
2026-04-11 15:12:13 +0300

    doing the credits requerments

---
+++ app/src/main/java/com/example/sqllite_shopping/Credits.java
@@ -0,0 +1,16 @@
+package com.example.sqllite_shopping;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
